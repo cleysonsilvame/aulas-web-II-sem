bfbe87d65e2a73ec2c32bafa112cfc45b1ca9811
Cleyson Silva <cleysonsilva.me@gmail.com>
2021-11-27 22:31:58 -0300

    minhas alterações da aula 14

--- aula-14/ExemplosCompletos/js/script02.js
+++ aula-14/ExemplosCompletos/js/script02.js
@@ -1,15 +1,13 @@
 "use strict";
 let url = "xml/menu.xml";
 $.ajax(url)
-    .done(function(xml){ //abertura do arquivo xml com sucesso
-        $(xml).find("opcao").each(
-            function(){
-                let vatributo = $(this).attr("id");  //obtem atributo
-                let vdado = $(this).text();  //obtem valor
-                $("#menu").append(vatributo + " - " + vdado + "<br>");
-            }
-        );
-    })
-    .fail(function(){ //falha na abertura do arquivo xml
-        alert("Ocorreu um erro no carregamento");
-    })
+  .done(xml => {
+    $(xml)
+      .find("opcao")
+      .each((_, element) => {
+        let vatributo = $(element).attr("id");
+        let vdado = $(element).text();
+        $("#menu").append(vatributo + " - " + vdado + "<br>");
+      });
+  })
+  .fail(() => alert("Ocorreu um erro no carregamento"));
