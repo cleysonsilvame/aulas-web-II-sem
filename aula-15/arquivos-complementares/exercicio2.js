a8c762988409f33a48519dece17ecf92fb253b98
Cleyson Silva <cleysonsilva.me@gmail.com>
2021-11-27 22:48:38 -0300

    arquivos complementares aula 15

---
+++ aula-15/arquivos-complementares/exercicio2.js
@@ -0,0 +1,47 @@
+var canais = [
+    {
+        "numero": 5,
+        "tipo": "Aberto",
+        "titulo": "Notícias do Mundo",
+        "descricao": "Programação variada durante o dia inteiro.",
+        "data_programa": "28/11/2021",
+        "hora_programa": "00:00 - 23:59",
+        "link": "http://webtv.com/canal.php?num=5"
+    },
+    {
+        "numero": 10,
+        "tipo": "Fechado",
+        "titulo": "Desenhos",
+        "descricao": "Programação infantil.",
+        "data_programa": "15/11/2021",
+        "hora_programa": "00:00 - 20:00",
+        "link": "http://webtv.com/canal.php?num=10"
+    },
+     {
+        "numero": 15,
+        "tipo": "Fechado",
+        "titulo": "Filmes",
+        "descricao": "Programação especial de lançamento de filmes.",
+        "data_programa": "08/12/2021",
+        "hora_programa": "14:00 - 23:00",
+        "link": "http://webtv.com/canal.php?num=15"
+    },
+    {
+        "numero": 20,
+        "tipo": "Aberto",
+        "titulo": "Culinária",
+        "descricao": "Programação com receitas baratas e rápidas.",
+        "data_programa": "25/12/2021",
+        "hora_programa": "10:00 - 12:00 / 17:00 - 18:00",
+        "link": "http://webtv.com/canal.php?num=20"
+    },  
+    {
+        "numero": 25,
+        "tipo": "Fechado",
+        "titulo": "Séries",
+        "descricao": "Programação especial de séries.",
+        "data_programa": "30/12/2021",
+        "hora_programa": "12:00 - 23:00",
+        "link": "http://webtv.com/canal.php?num=25"
+    }
+]
